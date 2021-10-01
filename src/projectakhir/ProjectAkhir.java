04fa0772f753d49573bff198ecdbe4d7154706b1
M Hanif Pratama <77886299+MHanifPratama@users.noreply.github.com>
2021-10-01 22:03:33 +0700

    Update ProjectAkhir.java

--- src/projectakhir/ProjectAkhir.java
+++ src/projectakhir/ProjectAkhir.java
@@ -5,47 +5,44 @@
  */
 package projectakhir;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
  *
  * @author USER
  */
-public class ProjectAkhir extends Application {
+public class Main extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("NasabahForm.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+//        try {
+//            NasabahDataModel database =  new NasabahDataModel("MYSQL");
+//            Individu a = new Individu (1l, 2l, "MHanifPratama", "BandarLampung",new Rekening(1234, 890000.000), 69);
+//            database.addNasabah(a);
+//            System.out.println("Sudah Ditambahkan");
+//        }   catch (SQLException ex) {
+//            System.out.println("Gagal Ditambahkan");
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+          launch(args);
     }
     
 }
